610024671fd0b33af734ac2b5ebceeb97c7a5d75
nikhilsutar81 <nikhilsutar81@gmail.com>
2026-02-19 14:00:54 +0530

    asd

--- client/src/pages/auth/AuthPage.tsx
+++ client/src/pages/auth/AuthPage.tsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router-dom"
 import { AuthView } from "@daveyplate/better-auth-ui"
+import { useEffect } from "react"
 
 export default function AuthPage() {
   const { pathname } = useParams()
+
+  useEffect(() => {
+    console.log('[AuthPage] Mounted with pathname:', pathname);
+    
+    // Intercept fetch to log auth requests
+    const originalFetch = window.fetch;
+    window.fetch = function(...args) {
+      const url = args[0]?.toString?.() || '';
+      if (url.includes('/auth')) {
+        console.log('[AuthPage] Fetching:', url, args[1]);
+      }
+      return originalFetch.apply(this, args as any).then((response) => {
+        if (url.includes('/auth')) {
+          console.log('[AuthPage] Response:', url, response.status, response.ok);
+        }
+        return response;
+      }).catch((error) => {
+        if (url.includes('/auth')) {
+          console.error('[AuthPage] Fetch error:', url, error);
+        }
+        throw error;
+      });
+    };
+  }, [pathname])
 
   return (
     <main className="p-6 flex flex-col justify-center items-center h-[80vh]">
